fcf150959589f6710088d72d70902f9e680fd58d
anish <anishgautam2024@gmail.com>
2024-11-08 22:43:00 +0545

    adjusted responsiveness

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-
+import { MdMenu } from "react-icons/md";
 
 const Nav = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -36,25 +36,25 @@ const Nav = () => {
         },
     ]
     return (
-        <div className="absolute z-10 w-full overflow-hidden">
+        <div className="absolute z-10 w-full ">
             <div className="bg-blue-500 text-white h-[22px] text-[12px] text-left pl-[10%] mb-2">
                 <p> <span className="text-[#fff9]"> Phone no:</span> +00 1234 567 or <span className="text-[#fff9]">  email us:</span> emailsample@email.com</p>
             </div>
             <div className="bg-[rgba(255,255,255,0.2)] text-white shadow-lg w-full h-[85px]">
-                <div className="container px-[10%] flex h-[85px] justify-between items-center">
+                <div className="relative px-[10%] flex h-[85px] justify-between items-center">
                     <Link to="/" className="text-[20px] font-bold uppercase">
                         Wave<span className="text-[#f3e53d]">Solution</span>
                     </Link>
                     <Link to=""
                         onClick={toggleMenu}
-                        className="lg:hidden text-gray-200 focus:outline-none"
+                        className="md:hidden text-gray-200 focus:outline-none"
                         aria-label="Toggle navigation"
                     >
-                        <span className="rotate-12 fa fa-bars">|||</span>
+                        <MdMenu />
                     </Link>
 
-                    <div className={`lg:flex ${isOpen ? 'block' : 'hidden'} w-full lg:w-auto`}>
-                        <ul className="lg:flex lg:space-x-6 mt-4 lg:mt-0 space-y-4 lg:space-y-0 text-lg">
+                    <div className={`hidden md:flex md:w-auto`}>
+                        <ul className="md:flex md:space-x-6 mt-4 md:mt-0 space-y-4 md:space-y-0 text-lg">
                             {
                                 menuitems.map((item) => (
                                     <li key={item.title}>
@@ -66,6 +66,22 @@ const Nav = () => {
                             }
                         </ul>
                     </div>
+                    {/* smaller device menu */}
+                    <div className={`${isOpen ? 'block absolute right-[14%] w-fit h-[100px] overflow-y-auto text-left top-[80%] py-4 px-10 rounded-md bg-[rgba(255,255,255,0.4)]' : "hidden"}`}>
+                        <ul className="text-black font-medium">
+                            {
+                                menuitems.map((item) => (
+                                    <li key={item.title} className="">
+                                        <Link to={item.link} className="hover:text-[#f3e53d] transition" >
+                                            {item.title}
+                                        </Link>
+                                    </li>
+                                ))
+                            }
+                        </ul>
+                    </div>
+
+
                 </div>
             </div>
         </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,12 +11,12 @@ const Home = () => {
             </div>
             <div className="relative top-0 left-0  bg-[rgba(0,0,0,0.3)] w-full h-[100vh]">
                 <div className="flex w-full h-full items-center z-99 pl-[10%]">
-                    <div className="relative space-y-3 h-[35%]">
+                    <div className="relative space-y-5 h-[35%]">
                         <h2 className="text-[#ffffffb3] font-semibold text-[18px]">Leave the house cleaning chores to us</h2>
                         <h1 className="text-[42px] leading-[50px] text-white font-medium">
                             Let us do the dirty work, so you <br /> don&apos;t have to.
                         </h1>
-                        <div className="space-x-5 absolute bottom-2 left-0">
+                        <div className="space-x-5">
                             <Button title="Appointment" bgcolor="#2b98f0" textcolor="white" hovertxt="#2b98f0" link="#" />
                             <Button title="call now" bgcolor="#f3e53d" textcolor="black" link="#" hovertxt="#f3e53d" />
                         </div>
