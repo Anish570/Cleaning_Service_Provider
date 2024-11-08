870dc57f7fe9954f3932c782db9f6c9f03133f38
anish <anishgautam2024@gmail.com>
2024-11-08 08:08:59 +0545

    button component ready

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,13 @@
+import { Link } from "react-router-dom"
 
 
-const Button = () => {
+const Button = ({ title = "click me", bgcolor = "#2b98f0", textcolor = "white", link = "#", hoverbgcolor = "transparent" }) => {
     return (
-        <div>Button</div>
+        <Link to={link}
+            className={`bg-[${bgcolor}] border-[2px] border-[${bgcolor}] text-[${textcolor}] hover:border-[${bgcolor}] hover:bg-[${hoverbgcolor}] hover:text-[${bgcolor}] tracking-[1px] px-6 py-2 rounded text-[12px] font-semibold uppercase`}
+        >
+            {title}
+        </Link>
     )
 }
 
